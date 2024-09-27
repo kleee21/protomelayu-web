ffdbbc822ce61e887a014e53ccb38041ef0ca1eb
Terraluminiscentia <cumabacafypdoang@gmail.com>
2024-09-27 22:48:01 +0700

    Update profil.js

--- profil.js
+++ profil.js
@@ -1,6 +1,7 @@
-const menuIcon = document.getElementById('menu-icon');
-const navLinks = document.getElementById('nav-links');
+const menuIcon = document.querySelector('.menu-icon');
+  const navLinks = document.querySelector('.nav-links');
 
-menuIcon.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
+  menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('show'); // Toggles the menu to show/hide
+    menuIcon.classList.toggle('change'); // Toggles the 3-strip to "X"
+  });
