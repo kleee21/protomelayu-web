48107fa940e278408b8b1ae8bd77fb4b152c16c2
Terraluminiscentia <cumabacafypdoang@gmail.com>
2024-09-29 17:30:10 +0700

    Update home.js

--- home.js
+++ home.js
@@ -10,3 +10,10 @@ window.addEventListener('load', function() {
       document.body.classList.add('loaded');
     }, 1000); // 2 seconds
   });
+const menuIcon = document.querySelector('.menu-icon');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('show'); // Toggles the menu to show/hide
+    menuIcon.classList.toggle('change'); // Toggles the 3-strip to "X"
+  });
