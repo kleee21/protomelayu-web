0b0144f113475f0a38f22dcec3f372104eeb4563
Terraluminiscentia <cumabacafypdoang@gmail.com>
2024-10-05 20:44:41 +0700

    Add files via upload

---
+++ activities.js
@@ -0,0 +1,18 @@
+// Wait until the page is fully loaded
+window.addEventListener('load', function() {
+    // Keep the loading screen visible for at least 2 seconds
+    setTimeout(function() {
+      // Hide the loading screen
+      const loadingScreen = document.getElementById('loading-screen');
+      loadingScreen.style.display = 'none';
+  
+      // Add the 'loaded' class to body to show the content
+      document.body.classList.add('loaded');
+    }, 1500); // 3 seconds
+  });
+  const menuIcon = document.querySelector('.menu-icon');
+  const navLinks = document.querySelector('.nav-links');
+  
+  menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('change'); // Toggles the 3-strip to "X"
+  });
