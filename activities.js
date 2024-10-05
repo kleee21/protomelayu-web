b0cf59c8041dee0a2664d0665ce7c31b21da0681
Terraluminiscentia <cumabacafypdoang@gmail.com>
2024-10-05 21:49:34 +0700

    Update activities.js

--- activities.js
+++ activities.js
@@ -14,5 +14,6 @@ window.addEventListener('load', function() {
   const navLinks = document.querySelector('.nav-links');
   
   menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('show'); // Toggles the menu to show/hide
     menuIcon.classList.toggle('change'); // Toggles the 3-strip to "X"
   });
